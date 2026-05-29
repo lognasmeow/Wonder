7fe120225bf11b25f4b06a93e684c0597ee5f80e
Lognas <42080354+lognasmeow@users.noreply.github.com>
2026-05-29 13:43:26 -0400

    clean up default player script

--- Player/player.gd
+++ Player/player.gd
@@ -6,16 +6,20 @@ const JUMP_VELOCITY = 15
 
 
 func _physics_process(delta):
-	# Add the gravity.
+	addGravity(delta)
+	handleJump()
+	handleMove()
+	move_and_slide()
+
+func addGravity(delta: float) -> void:
 	if not is_on_floor():
 		velocity += (get_gravity() * 4) * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+func handleJump() -> void:
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+func handleMove() -> void:
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -24,5 +28,3 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
