109480afaae1190587c6256d51599f2d346d61d5
Lognas <42080354+lognasmeow@users.noreply.github.com>
2026-05-30 16:23:54 -0400

    randomize which modifier is grabbed

--- Globals/modifiers.gd
+++ Globals/modifiers.gd
@@ -1,9 +1,19 @@
 extends Node
 
-var jumpHeightMultiplier: float = 1
-var invertedWalking: bool = false
+var jumpHeightMultiplier: float
+var invertedWalking: bool
+var modifierPaths: Array
 
+func _ready():
+	resetModifiers()
+	resetModifierPaths()
 
 func resetModifiers() -> void:
 	jumpHeightMultiplier = 1
 	invertedWalking = false
+
+func resetModifierPaths() -> void:
+	modifierPaths.clear()
+	modifierPaths = ["res://Modifiers/DoubleJumpHeight/Mod_IncreasedJumpHeight.tscn",
+					"res://Modifiers/InvertedWalking/Mod_InvertedWalking.tscn",
+					]
--- Maps/map_main.gd
+++ Maps/map_main.gd
@@ -16,6 +16,7 @@ func _on_player_entered_fall_plane():
 	transitioningOut.emit()
 	await get_tree().create_timer(1.0).timeout
 	Modifiers.resetModifiers()
+	Modifiers.resetModifierPaths()
 	get_tree().reload_current_scene()
 
 
@@ -25,7 +26,12 @@ func _on_player_entered_finish_line():
 	player.global_position = playerSpawnPoint.global_position
 	transitioningIn.emit()
 	await get_tree().create_timer(0.5).timeout
-	var temp = preload("res://Modifiers/DoubleJumpHeight/Mod_DoubleJumpHeight.tscn").instantiate()
-	add_child(temp)
-	var temp2 = preload("res://Modifiers/InvertedWalking/Mod_InvertedWalking.tscn").instantiate()
-	add_child(temp2)
+	applyRandomModifier()
+
+func applyRandomModifier() -> void:
+	if (Modifiers.modifierPaths.size() > 0):
+		var randValue: int = randi_range(0, Modifiers.modifierPaths.size() - 1)
+		var randModifierPath: String = Modifiers.modifierPaths[randValue]
+		var modifier = load(randModifierPath).instantiate()
+		add_child(modifier)
+		Modifiers.modifierPaths.remove_at(randValue)
